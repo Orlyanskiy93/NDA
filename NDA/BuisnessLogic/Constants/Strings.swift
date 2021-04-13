fea24021e3f45501d6b914b8c1c5113f8aed5ec8
Dmitriy <dmitri.orlyanskiy@mail.ru>
2021-04-13 19:26:30 +0300

    add localizable strings

--- NDA/BuisnessLogic/Constants/Strings.swift
+++ NDA/BuisnessLogic/Constants/Strings.swift
@@ -8,46 +8,59 @@
 import Foundation
 
 extension String {
+    
+    static let welcomeMessage = NSLocalizedString("welcomeMessage", comment: "")
+    
     struct Error {
-        static let error = "Error"
-        static let ok = "OK"
-        static let wrongName = "Name must contains only letters"
-        static let wrongAge = "Age is to much"
-        static let wrongEmail = "Wrong email"
-        static let emptyField = "Field is empty"
-        static let saveUser = "Unable to save user"
-        static let getUser = "Unable to load user"
-        static let optionsCount = "Wrong count of options"
-        static let tryAgain = "Try again"
+        static let error = NSLocalizedString("error", comment: "")
+        static let ok = NSLocalizedString("ok", comment: "")
+        static let wrongName = NSLocalizedString("wrongName", comment: "")
+        static let wrongAge = NSLocalizedString("wrongAge", comment: "")
+        static let wrongEmail = NSLocalizedString("wrongEmail", comment: "")
+        static let emptyField = NSLocalizedString("emptyField", comment: "")
+        static let saveUser = NSLocalizedString("saveUser", comment: "")
+        static let getUser = NSLocalizedString("getUser", comment: "")
+        static let optionsCount = NSLocalizedString("optionsCount", comment: "")
+        static let tryAgain = NSLocalizedString("tryAgain", comment: "")
+        static let rightAnswer = NSLocalizedString("rightAnswer", comment: "")
+        static let shortText = NSLocalizedString("shortText", comment: "")
     }
     
     struct Button {
-        static let ok = "OK"
-        static let `continue` = "CONTINUE"
-        static let next = "NEXT"
-        static let begin = "BEGIN"
-        static let editInformation = "EDIT INFORMATION"
-        static let eraceAppData = "ERACE APP DATA"
+        static let ok = NSLocalizedString("ok", comment: "")
+        static let `continue` = NSLocalizedString("continue", comment: "")
+        static let next = NSLocalizedString("next", comment: "")
+        static let begin = NSLocalizedString("begin", comment: "")
+        static let editInformation = NSLocalizedString("editInformation", comment: "")
+        static let eraceAppData = NSLocalizedString("eraceAppData", comment: "")
     }
     
     struct Home {
-        static let questionnaireDescription = "Please answer the next few questions as quickly as your ability. The questionnaire will consist of 3 parts: Self - Efficacy, Short - Answer and Long - Answer questions."
-        static let beginQuestionnaire = "BEGIN QUESTIONNAIRE"
+        static let questionnaireDescription = NSLocalizedString("questionnaireDescription", comment: "")
+        static let beginQuestionnaire = NSLocalizedString("beginQuestionnaire", comment: "")
     }
     
     struct PartOne {
-        static let title = "Part 1: Self Efficacy"
-        static let moduleDescription = "The following questions are designed to help us get a better understanding of any symptoms you might be experiencing in the areas of attention, memory, organization and thinking. Please try to objective in your answers."
-        static let instructions = "Using the scale provided, please rate how certain you are can do the following actions:"
-        static let cannotCertain = "Cannot do at all"
-        static let moderatelyCertain = "Moderately can do"
-        static let highlyCertain = "Highly certain can do"
+        static let title = NSLocalizedString("partOneTitle", comment: "")
+        static let moduleDescription = NSLocalizedString("partOneModuleDescription", comment: "")
+        static let instructions = NSLocalizedString("instructions", comment: "")
+        static let cannotCertain = NSLocalizedString("cannotCertain", comment: "")
+        static let moderatelyCertain = NSLocalizedString("moderatelyCertain", comment: "")
+        static let highlyCertain = NSLocalizedString("highlyCertain", comment: "")
+        static let question1 = NSLocalizedString("partOneQuestion1", comment: "")
+        static let question2 = NSLocalizedString("partOneQuestion2", comment: "")
+        static let question3 = NSLocalizedString("partOneQuestion3", comment: "")
+        static let question4 = NSLocalizedString("partOneQuestion4", comment: "")
+        static let question5 = NSLocalizedString("partOneQuestion5", comment: "")
+        static let question6 = NSLocalizedString("partOneQuestion6", comment: "")
+        static let question7 = NSLocalizedString("partOneQuestion7", comment: "")
+        static let question8 = NSLocalizedString("partOneQuestion8", comment: "")
     }
     
     struct PartTwo {
-        static let title = "Part 2: Short Answer"
-        static let moduleDescription = "The following questions are designed to test you using a few simple short - answer questions. Please try to answer these questions quickly and correctly."
-        static let arythmeticQuestion = "What is "
-        static let identifyQuestion = "Please identify picture of "
+        static let title = NSLocalizedString("partTwoTitle", comment: "")
+        static let moduleDescription = NSLocalizedString("partTwoModuleDescription", comment: "")
+        static let arythmeticQuestion = NSLocalizedString("arythmeticQuestion", comment: "")
+        static let identifyQuestion = NSLocalizedString("identifyQuestion", comment: "")
     }
 }
